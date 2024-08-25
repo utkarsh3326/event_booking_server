d025281df4748fe079882073e7044f717c0bc70e
Utkarsh Agarwal <utagarwal687@gmail.com>
2024-08-25 17:06:48 +0530

    Added tasks definition and policy files

--- event_service/config.js
+++ event_service/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     PORT: process.env.PORT || 5001,
-    MONGO_URI: "mongodb+srv://event_booking:EventBooking%402024@cluster0.4m0yk.mongodb.net/eventdb?retryWrites=true&w=majority&appName=Cluster0",
+    MONGO_URI: process.env.MONGO_URI,
     REDIS_URL: process.env.REDIS_URL,
     ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
 };
--- user_service/config.js
+++ user_service/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 5000,
-  MONGO_URI: "mongodb+srv://event_booking:EventBooking%402024@cluster0.4m0yk.mongodb.net/userdb?retryWrites=true&w=majority&appName=Cluster0",
-  REDIS_URL: process.env.REDIS_URL, 
+  MONGO_URI: process.env.MONGO_URI,
+  REDIS_URL: process.env.REDIS_URL,
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
   ACCESS_TOKEN_EXPIRE: process.env.ACCESS_TOKEN_EXPIRE || '15m',
